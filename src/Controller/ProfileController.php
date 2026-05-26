5a6ffea9a65051d8cb03b97e2cbf2f0f600a84e1
SaharTabarki <sahouratabarki@gmail.com>
2026-05-27 02:04:17 +0300

    profile +editprofile trival pour test

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Form\EditProfileType;
 
 class ProfileController extends AbstractController
 {
@@ -19,4 +22,25 @@ return $this->render('auth/profile.html.twig', [
 'user' => $user,
 ]);
 }
+
+#[Route('/profile/edit', name: 'app_profile_edit')]
+    public function edit(Request $request, EntityManagerInterface $em)
+{
+    $user = $this->getUser();
+
+    $form = $this->createForm(EditProfileType::class, $user);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+        $em->flush(); // Symfony met à jour automatiquement
+
+        return $this->redirectToRoute('app_profile');
+    }
+
+    return $this->render('auth/edit_profile.html.twig', [
+        'form' => $form->createView()
+    ]);
 }
+}
+
---
+++ src/Form/EditProfileType.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EditProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('bio', TextareaType::class, [
+                'required' => false
+            ]);
+    }
+}
