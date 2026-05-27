e52426b64b6a99dbd1242eb7ebfc4aae75c50828
Selmi-Malek <malek.selmi@insat.ucar.tn>
2026-05-27 17:20:34 +0200

    modifications favoris

--- src/DataFixtures/RoomFixtures.php
+++ src/DataFixtures/RoomFixtures.php
@@ -8,7 +8,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-class AppFixtures extends Fixture
+class RoomFixtures extends Fixture
 {
     public function __construct(
         private UserPasswordHasherInterface $hasher
